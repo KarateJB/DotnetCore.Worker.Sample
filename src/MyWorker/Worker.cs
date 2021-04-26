3ced10c79c5f7d9091e1116579263041198aaaa0
KarateJB <ppippenkimo@gmail.com>
2021-04-26 11:18:14 +0800

    Complete sample

--- src/MyWorker/Worker.cs
+++ src/MyWorker/Worker.cs
@@ -1,28 +1,40 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MyWorker.Models.Config;
+using MyWorker.Services;
 
 namespace MyWorker
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly AppSettings _appSettings;
+        private readonly ISyncDataService _syncDataService;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(
+            ILogger<Worker> logger,
+            IOptions<AppSettings> configuration,
+            ISyncDataService syncDataService)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._appSettings = configuration.Value;
+            this._syncDataService = syncDataService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                this._logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                // Business logic
+                await this._syncDataService.StartAsync();
+
+                await Task.Delay(3000, stoppingToken);
             }
         }
     }
